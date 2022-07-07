89e319144b46e3c18972ac91d7c08a55762d3ddb
Dave Brenner <dbrenner101@gmail.com>
2022-07-07 15:36:49 -0400

    Added javadoc, consolidated redundant code and removed unused code

--- SavingsGoalsUI/src/main/java/com/brenner/savingsgoals/view/ViewFactory.java
+++ SavingsGoalsUI/src/main/java/com/brenner/savingsgoals/view/ViewFactory.java
@@ -11,6 +11,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 
+/**
+ * Class to handle initializing controllers for individual views and then making them visible. Also handles swapping
+ * from one view to another.
+ */
 public class ViewFactory {
     private Parent displayedView;
     private AnchorPane targetPane;
@@ -22,78 +26,114 @@ public class ViewFactory {
         this.savingsGoalManager = savingsGoalManager;
     }
     
+    /**
+     * Method to call when application first starts. The main window contains the menu and anchor pane to display the
+     * active scene.
+     */
     public void showMainWindow() {
         MainWindowController mainWindowController = new MainWindowController(this.savingsGoalManager, this, "mainWindow.fxml");
-        //BaseController baseController = new MainWindowControllerOld(this.savingsGoalManager, this, "mainWindowOld.fxml");
         initializeStage(mainWindowController);
         this.targetPane = mainWindowController.getRootPane();
     }
     
+    /**
+     * Shows the scene to enter allocation values for goals
+     */
     public void showDepositAllocation() {
         BaseController baseController = new DepositAllocationController(this.savingsGoalManager, this, "allocateDepositView.fxml");
         initView(baseController);
     }
     
+    /**
+     * Displays the scene with the list of transactions
+     */
     public void showTransactionList() {
         BaseController baseController = new TransactionsListController(this.savingsGoalManager, this, "transactionsList.fxml");
         initView(baseController);
     }
     
+    /**
+     * Displays the scene for adding or editing a transaction (only adding is currently supported)
+     */
     public void showAddEditTransaction() {
         BaseController baseController = new AddEditTransactionController(this.savingsGoalManager, this, "addEditTransaction.fxml");
         initView(baseController);
     }
     
+    /**
+     * Displays the scene with the list of savings goals and their information
+     */
     public void showSavingsGoalList() {
         BaseController baseController = new SavingsGoalTableController(this.savingsGoalManager, this, "savingsGoalsTableView.fxml");
         initView(baseController);
     }
     
+    /**
+     * Displays the scene for adding or updating a savings goal
+     */
     public void showAddUpdateSavingsGoals() {
         BaseController baseController = new AddUpdateSavingsGoalController(this.savingsGoalManager, this, "addSavingsGoal.fxml");
         initView(baseController);
-        //initializeStage(baseController);
     }
     
+    /**
+     * Displays a scene with the list of deposits waiting to be allocated.
+     */
     public void showDepositsList() {
         BaseController baseController = new DepositsListController(this.savingsGoalManager, this, "depositsList.fxml");
         initView(baseController);
     }
     
+    /**
+     * Displays the add/update deposit scene.
+     */
     public void showAddUpdateDeposits() {
         BaseController baseController = new AddUpdateDepositController(this.savingsGoalManager, this, "addDeposit.fxml");
         initView(baseController);
-        //this.screenController.addScreen("addUpdateDeposits", scene);
-        //initializeStage(baseController);
     }
     
+    /** method called to close a stage.
+     *
+     *  @param stageToClose The stage to close
+     */
     public  void closeStage(Stage stageToClose){
         
         stageToClose.close();
-        //activeStages.remove(stageToClose);
     }
     
+    /**
+     * Initializes the controller and loads the FXML to build the scene.
+     *
+     * @param baseController The controller instance with the named FXML file
+     */
     private void initView(BaseController baseController) {
-        URL file = getClass().getResource(baseController.getFxmlName());
-        FXMLLoader fxmlLoader = new FXMLLoader(file);
-        fxmlLoader.setController(baseController);
-        Parent viewPane = null;
-        try {
-            viewPane = fxmlLoader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Parent viewPane = loadParentFromFxml(baseController);
     
         if (this.displayedView != null) {
             this.targetPane.getChildren().remove(this.displayedView);
         }
-        /*this.stage.getScene().getWindow().setHeight(viewPane.getLayoutX());
-        this.stage.getScene().getWindow().setWidth(viewPane.getLayoutY());*/
+        
         this.targetPane.getChildren().add(viewPane);
         this.displayedView = viewPane;
     }
     
+    /**
+     * USed to initialize the main window and set the stage.
+     * @param baseController
+     */
     private void initializeStage(BaseController baseController){
+        Parent parent = loadParentFromFxml(baseController);
+        
+        Scene scene = new Scene(parent);
+        
+        stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+        
+        baseController.postInitialization();
+    }
+    
+    private Parent loadParentFromFxml(BaseController baseController) {
         URL file = getClass().getResource(baseController.getFxmlName());
         FXMLLoader fxmlLoader = new FXMLLoader(file);
         fxmlLoader.setController(baseController);
@@ -104,12 +144,6 @@ public class ViewFactory {
             e.printStackTrace();
         }
         
-        Scene scene = new Scene(parent);
-        
-        stage = new Stage();
-        stage.setScene(scene);
-        stage.show();
-        
-        baseController.postInitialization();
+        return parent;
     }
 }
