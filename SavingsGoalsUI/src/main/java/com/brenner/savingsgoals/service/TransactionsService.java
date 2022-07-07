1e336c521b2ccd5ba826a3dcb1758095a55fc4c2
Dave Brenner <dbrenner101@gmail.com>
2022-07-07 15:55:26 -0400

    added javadoc

--- SavingsGoalsUI/src/main/java/com/brenner/savingsgoals/service/TransactionsService.java
+++ SavingsGoalsUI/src/main/java/com/brenner/savingsgoals/service/TransactionsService.java
@@ -7,12 +7,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * RESTful client for the Transactions API
+ */
 public class TransactionsService extends AbstractServiceManager {
     
-    ObjectMapper mapper = new ObjectMapper();
+    private ObjectMapper mapper = new ObjectMapper();
     
     private static final String TRANSACTIONS_BASE_URL = "/transactions";
     
+    /**
+     * Calls the REST endpoint supporting retrival of the transactions list.
+     *
+     * @return The List of transaction objects
+     */
     public List<Transaction> retrieveTransactions() {
         try {
             String response = super.doGet(TRANSACTIONS_BASE_URL);
@@ -22,6 +30,12 @@ public class TransactionsService extends AbstractServiceManager {
         }
     }
     
+    /**
+     * Calls the REST endpoint for retrieving a single transaction.
+     *
+     * @param transactionId The transaction unique identifier
+     * @return The Transaction object
+     */
     public Transaction getTransaction(Long transactionId) {
         try {
             String response = super.doGet(TRANSACTIONS_BASE_URL + "/" + transactionId);
@@ -31,6 +45,12 @@ public class TransactionsService extends AbstractServiceManager {
         }
     }
     
+    /**
+     * Calls the REST endpoint to save a new transaction.
+     *
+     * @param transaction The transaction to save
+     * @return The transaction returned back from the API
+     */
     public Transaction saveNewTransaction(Transaction transaction) {
         try {
             String response = super.doPost(TRANSACTIONS_BASE_URL, mapper.writeValueAsString(transaction));
@@ -40,6 +60,12 @@ public class TransactionsService extends AbstractServiceManager {
         }
     }
     
+    /**
+     * Call to the REST endpoint to update a transaction
+     *
+     * @param transaction The transaction to update including the unique identifier
+     * @return The Transaction returned from the API
+     */
     public Transaction updateTransaction(Transaction transaction) {
         try {
             String response = super.doPut(TRANSACTIONS_BASE_URL + "/" + transaction.getTransactionId(), mapper.writeValueAsString(transaction));
@@ -49,6 +75,12 @@ public class TransactionsService extends AbstractServiceManager {
         }
     }
     
+    /**
+     * API call to delete a transaction.
+     *
+     * @param transactionId The transaction unique identifer.
+     * @return The HTTP status. The API does not return a value
+     */
     public int deleteTransaction(Long transactionId) {
         try {
             int response = super.doDelete(TRANSACTIONS_BASE_URL + "/" + transactionId, null);
