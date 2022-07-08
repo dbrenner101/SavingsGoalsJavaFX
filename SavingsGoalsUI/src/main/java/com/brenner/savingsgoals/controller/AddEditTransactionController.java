ccb08ca003bdd7b4558cdcbf2d0831086b667534
Dave Brenner <dbrenner101@gmail.com>
2022-07-07 20:25:58 -0400

    Added field validation

--- SavingsGoalsUI/src/main/java/com/brenner/savingsgoals/controller/AddEditTransactionController.java
+++ SavingsGoalsUI/src/main/java/com/brenner/savingsgoals/controller/AddEditTransactionController.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.util.StringConverter;
+import net.synedra.validatorfx.Validator;
 
 import java.net.URL;
 import java.text.ParseException;
@@ -21,10 +22,10 @@ import java.util.ResourceBundle;
  * Controller to handle the add (edit) transaction.
  *
  * Relies on the addEditTransaction.fxml
- *
- * @TODO add validator
  */
 public class AddEditTransactionController extends BaseController implements Initializable {
+    
+    Validator validator = new Validator();
     
     @FXML
     private TextField amountTextField;
@@ -50,16 +51,20 @@ public class AddEditTransactionController extends BaseController implements Init
     
     @FXML
     void saveButtonAction(ActionEvent event) throws ParseException {
-        Date transactionDate = CommonUtils.parseStdFormatDateString(this.datePicker.getEditor().getText());
-        SavingsGoal fromGoal = fromGoalSelection.getSelectionModel().getSelectedItem().getSavingsGoal();
-        SavingsGoal toGoal = toGoalSelection.getSelectionModel().getSelectedItem().getSavingsGoal();
-        Float amount = Float.valueOf(amountTextField.getText());
-        String notes = notesTextArea.getText();
+        validator.validate();
         
-        Transaction t = new Transaction(null, transactionDate, fromGoal, toGoal, amount, notes);
-        this.savingsGoalManager.addTransaction(t);
-        
-        viewFactory.showTransactionList();
+        if (! validator.containsErrors()) {
+            Date transactionDate = CommonUtils.parseStdFormatDateString(this.datePicker.getEditor().getText());
+            SavingsGoal fromGoal = fromGoalSelection.getSelectionModel().getSelectedItem().getSavingsGoal();
+            SavingsGoal toGoal = toGoalSelection.getSelectionModel().getSelectedItem().getSavingsGoal();
+            Float amount = Float.valueOf(amountTextField.getText());
+            String notes = notesTextArea.getText();
+            
+            Transaction t = new Transaction(null, transactionDate, fromGoal, toGoal, amount, notes);
+            this.savingsGoalManager.addTransaction(t);
+            
+            viewFactory.showTransactionList();
+        }
     }
     
     private void initChoiceBoxes() {
@@ -91,8 +96,53 @@ public class AddEditTransactionController extends BaseController implements Init
         });
     }
     
+    private void buildValidation() {
+        validator.createCheck().dependsOn("fromGoal", fromGoalSelection.valueProperty())
+                .withMethod(c -> {
+                    SavingsGoalModel selectedItem = c.get("fromGoal");
+                    if (selectedItem == null) {
+                        c.error("From goal is required.");
+                    }
+                }).decorates(fromGoalSelection);
+    
+        validator.createCheck().dependsOn("toGoal", toGoalSelection.valueProperty())
+                .withMethod(c -> {
+                    SavingsGoalModel selectedItem = c.get("toGoal");
+                    if (selectedItem == null) {
+                        c.error("To goal is required.");
+                    }
+                }).decorates(toGoalSelection);
+        
+        validator.createCheck().dependsOn("date", datePicker.getEditor().textProperty())
+                .withMethod(c -> {
+                    String date = c.get("date");
+                    if (date.trim().length() == 0) {
+                        c.error("Transaction date is required.");
+                    }
+                    try {
+                        CommonUtils.parseStdFormatDateString(date);
+                    } catch (ParseException pe) {
+                        c.error("A valid transaction date is required.");
+                    }
+                }).decorates(datePicker.getEditor());
+        
+        validator.createCheck().dependsOn("amount", amountTextField.textProperty())
+                .withMethod(c -> {
+                    String amount = c.get("amount");
+                    if (amount.trim().length() == 0) {
+                        c.error("Amount is required.");
+                    }
+                    try {
+                        Float.parseFloat(amount);
+                    } catch (NumberFormatException nfe) {
+                        c.error("A valid number is required for amount.");
+                    }
+                }).decorates(amountTextField);
+    }
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initChoiceBoxes();
+        buildValidation();
     }
 }
